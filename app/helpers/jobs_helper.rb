c127155490c0deb8acd00daf25c08be7c4be11e5
Omer Rauchwerger <omer@rauchy.net>
2012-10-22 07:05:26 +0200

    jobs#index styling

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,2 +1,5 @@
 module JobsHelper
+  def url_with_protocol(url)
+    /^http/.match(url) ? url : "http://#{url}"
+  end
 end
--- spec/helpers/jobs_helper_spec.rb
+++ spec/helpers/jobs_helper_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe JobsHelper do
+  describe "#url_with_protocol" do
+    it "appends the protocol when absent" do
+      helper.url_with_protocol("www.google.com").should == "http://www.google.com"
+    end
+
+    it "does not append the protocol when present" do
+      helper.url_with_protocol("http://www.google.com").should == "http://www.google.com"
+    end
+  end
 end
