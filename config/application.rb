6f7b3dd336c45f836dc622ceda96de6f2bf453af
Omer Rauchwerger <omer@rauchy.net>
2012-10-23 13:17:25 +0200

    Fix for Heroku's activeadmin issue

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module RubyUnderground
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+    #Added to fix devise/active admin issue ?
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
