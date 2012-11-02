b63ef9573d36bc8152e8feffa7690764c2ab5734
Omer Rauchwerger <omer@rauchy.net>
2012-11-02 14:32:31 +0200

    Events scoped by event time by default

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :lat, :lon, :name, :time, :event_url, :meetup_id
   scope :upcoming, -> { where("time > ?", Time.now) }
+  default_scope order('time')
 
   def self.next
     upcoming.first
