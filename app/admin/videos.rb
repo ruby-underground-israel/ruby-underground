784ef0199c95ea1e3024c10fd591e2211d353d5a
Omer Rauchwerger <omer@rauchy.net>
2012-12-21 10:22:55 +0200

    Exposing youtube_id in Videos admin page

--- app/admin/videos.rb
+++ app/admin/videos.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Video do
-  
+  form do |f|
+    f.inputs "Details" do
+      f.input :description
+      f.input :thumbnail
+      f.input :title
+      f.input :youtube_id
+    end
+    f.buttons
+  end
 end
